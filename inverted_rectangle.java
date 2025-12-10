34efdd255870e142c3ad592c7612a00106ad8160
Sujit <141266117+Thesujit@users.noreply.github.com>
2025-12-10 12:03:13 +0530

    Add files via upload

---
+++ inverted_rectangle.java
@@ -0,0 +1,13 @@
+public  class inverted_rectangle {
+    public static void inverted_rectangle ( int rows, int cols ) {
+        for (  int i=1; i<=rows ; i++) {
+            for (int j = 1; j <=rows-1; j++ ){
+                System.out.print("*");
+            }
+            }
+            System.out.println("");
+        }
+    }
+public static void main(String[] args) {
+    
+}
