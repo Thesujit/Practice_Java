525c488c5853134703367d19e0a77ed9bd21417b
Sujit <141266117+Thesujit@users.noreply.github.com>
2025-12-11 14:08:31 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,11 @@
+public class array {
+    public static int  arrayL (int num , int key){
+        for (int i =0; i<= num.Length; i++); {
+            if(num[i] == key;) {
+                return i;
+            }
+            return -1;
+        }
+public static void main (String args[])
+    }
+}
