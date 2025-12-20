79b811c559a1d96ebb78bce5587c4063774c0f82
Sujit <141266117+Thesujit@users.noreply.github.com>
2025-12-20 20:11:12 +0530

    Add files via upload

---
+++ array2d.java
@@ -0,0 +1,7 @@
+public class array2d {
+    public static void main(String[] args) {
+        int matrix[][] = new int[3][3];
+        int m = matrix.length;
+        int n = matrix[0].length;
+    }
+}
