aa7335b3f8121798309998d38d55971fd8506232
Sujit <141266117+Thesujit@users.noreply.github.com>
2026-02-26 12:25:53 +0530

    Add files via upload

---
+++ doubleLL.java
@@ -0,0 +1,69 @@
+public class doubleLL {
+    public class Node {
+        int data; 
+        Node next;
+        Node prev;
+
+
+        public Node(int data){
+        this.data= data;
+        this.next = null;
+        this.prev = null;
+    }
+    }
+
+public static Node head;
+public static Node tail;
+public static int size;
+//add 
+public void addfirst(int data){
+    Node newNode = new Node(data) ;
+    size++; 
+    if(head == null){
+        head= tail = newNode;
+        return;
+    }
+    newNode.next = head;
+    head.prev = newNode;
+    head= newNode;
+}
+//print
+public void print(){
+    Node temp= head;
+    while(temp!= null){
+        System.out.print(temp.data + "<->");
+        temp = temp.next;
+    }
+    System.out.println("null");
+}
+//remove - removelast
+public int removefirst(){
+    if (head == null){
+        System.out.println("DLL is empty");
+        return Integer.MIN_VALUE;
+    }
+    if(size ==1) {
+        int val = head.data;
+        head= tail=null;
+        size --;
+        return val;
+    }
+    int val = head.data;
+    head= head.next;
+    head.prev = null;
+    size--;
+    return val;
+}
+public static void main (String args[]){
+    doubleLL dll= new doubleLL();
+    dll.addfirst(3);
+    dll.addfirst(2);
+    dll.addfirst(1);
+    dll.print();
+    System.out.println(dll.size);
+    dll.removefirst();
+    dll.print();
+    System.out.print(dll.size);
+}
+    
+}
