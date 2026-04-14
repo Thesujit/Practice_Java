7b67206728b21c3b4c050ca2fb812358761b937a
Sujit <141266117+Thesujit@users.noreply.github.com>
2026-04-14 14:18:10 +0530

    diameter of tree opt

--- diameteroftreeopt.java
+++ diameteroftreeopt.java
@@ -40,4 +40,15 @@ public class dimeteroftreeopt {
 
         return new Info(diam, ht)
     }
+    public static void main(String args[]){
+        Node root = new Node(1);
+        root.left= new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        System.out.println(diameter(root).diam);
+    }
 }
