3bbeb9991327f0af611b627e602077d9db9b5ccf
Sujit <141266117+Thesujit@users.noreply.github.com>
2026-04-16 12:53:16 +0530

    Optimized approach lowest common ancestor

--- lowestcommon.java
+++ lowestcommon.java
@@ -34,6 +34,25 @@ public class lowestcommon{
         Node lca = path1.get(i-1);
         return lca;
     }
+
+    public static Node lca2(Node root, int n1, int n2){
+        if(root == null ||root.data == n1 || root.data == n2){
+            return root;
+        }
+        Node leftLca = lca2(root.left, n1,n2);
+        Node rightLca = lca2(root.right, n1,n2);
+
+        // left lca = val right lca = null 
+        if(rightLca == null){
+            return leftLca;
+        }
+        if(leftLca == null){
+            return rightLca;
+        }
+        return root;
+    }
+
+
     public static void main(String args[]){
         Node root = new Node(1);
         root.left= new Node(2);
@@ -43,6 +62,6 @@ public class lowestcommon{
         root.right.left = new Node(6);
         root.right.right = new Node(7);
 
-        int n1= 4, n2=5;
-        System.out.println(lca(root,n1,n2));
+        int n1= 4, n2=7;
+        System.out.println(lca(root, n1, n2).data);
 }
