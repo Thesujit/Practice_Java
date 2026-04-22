84a18f92d4573acfd43b7ae8994df4d1f28bbf7f
Sujit <141266117+Thesujit@users.noreply.github.com>
2026-04-22 16:03:19 +0530

    Binary Search

--- binaryserarchtree.java
+++ binaryserarchtree.java
@@ -78,6 +78,22 @@ public class binaryserarchtree{
         return root;
     }
 
+    public static void printInRange(Node root, int k1, int k2){
+        if(root == null){
+            return ;
+        }
+
+        if(root.data >= k1 && root.data <= k2){
+            printInRange(root.left,k1,k2);
+            System.out.println(root.data+" ");
+            printInRange(root.right, k1,k2);
+        }else if(root.data<k1){
+            printInRange(root.left,k1,k2);
+        }else{
+            printInRange(root.right,k1,k2);
+        }
+    }
+
     public static void main(String args[]){
         int values[]={5,1,3,4,2,7};
         Node root= null;
@@ -96,6 +112,8 @@ public class binaryserarchtree{
         root = delete(root,1);
         System.out.println();
         inorder (root);
+
+        printInRange(root, 5,7);
     }
 
 }
